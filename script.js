664764503c2f838ffbeb81dc7e1e4d679af8840b
Jiahui Wang <wangjiahui@WangJiahuis-MacBook-Pro.local>
2019-11-06 21:41:20 -0800

    fixed movie year issue, add another if else statement

--- script.js
+++ script.js
@@ -14,128 +14,133 @@ M.Slider.init(slider, {
     interval:3000
 });
 // -------------------------------------
-    var foodBtnEl= [];
-    var beerYear= [];
+var foodBtnEl= [];
+var beerYear= [];
 
-    $("#search-btn").on("click", function(event){
-        event.preventDefault();
-        var newFood = $("#food-input").val().trim();
-        foodBtnEl.push(newFood);
-        getBeer(newFood);
-    });
+$("#search-btn").on("click", function(event){
+    event.preventDefault();
+    var newFood = $("#food-input").val().trim();
+    foodBtnEl.push(newFood);
+    getBeer(newFood);
+});
 
-    function arrayButtons() {
-        $("#button-view").empty();
-        for (var i= 0; i < foodBtnEl.length; i++) {
-            var buttonEl = $("<button>");
+function arrayButtons() {
+    $("#button-view").empty();
+    for (var i= 0; i < foodBtnEl.length; i++) {
+        var buttonEl = $("<button>");
 
-            buttonEl.addClass("food-btn");
-            buttonEl.attr("data-name", foodBtnEl[i]);      
-            buttonEl.attr("data-year", beerYear[i]);            
-            buttonEl.text(foodBtnEl[i]);
+        buttonEl.addClass("food-btn");
+        buttonEl.attr("data-name", foodBtnEl[i]);      
+        buttonEl.attr("data-year", beerYear[i]);            
+        buttonEl.text(foodBtnEl[i]);
 
-            $("#button-view").prepend(buttonEl);
-        }
-    }   
-    // google "attach event handler to a dynamic button"
-    $(document).on("click", ".food-btn", function(){ 
-        var newFood= ($(this).attr("data-name")); 
-        getBeer(newFood);
-    });
+        $("#button-view").prepend(buttonEl);
+    }
+}   
+// google "attach event handler to a dynamic button"
+$(document).on("click", ".food-btn", function(){ 
+    var newFood= ($(this).attr("data-name")); 
+    getBeer(newFood);
+});
 
-    function getBeer(newFood) { 
-        var userInput = newFood;
-        var queryURL = "https://api.punkapi.com/v2/beers/?food=" + userInput;
-        $.ajax({
-            url: queryURL,
-            method: "GET"         
-        }).then(function(response) {
-            var random = Math.floor(Math.random()*response.length);
-
-            var year = response[random].first_brewed.slice(3);
-            // there's a var in the updatebber funciton that has the same value
-            beerYear.push(year);
-
-            arrayButtons(); 
-            updateBeer(response, random);
-            getMovie(year);            
-        });
-    }        
-
-    function updateBeer(response,random) {
-        $("#beer-view").empty();
-
-        var beerDiv = $("<div class= 'beer-data'>");
-    
-        var name = response[random].name;                      
-        var description = response[random].description;
-        var foodPairing = response[random].food_pairing;
-        var firstBrewedYear = response[random].first_brewed;
-        if (firstBrewedYear.length === 7){
-            firstBrewedYear = response[random].first_brewed.slice(3);
-        } else{
-            firstBrewedYear = response[random].first_brewed;
-        }
-        console.log(response);
-        console.log(response[random].first_brewed);
-
-        var pOne = $("<h5>").html("Beer Name :  " + name);
-        var pTwo = $("<h6>").html("First-brewed Year :  " + firstBrewedYear);           
-        var pThree = $("<h6>").html("Description : " +  '<br>' + description);
-        var pFour = $("<h6>").html("Food Paring : " +  '<br>' + foodPairing);
-        // null not working
-        var imgURL = response[random].image_url;
-        if (imgURL === null){
-            var image = $("<p>").text("Beer picture unavailable");
-            image.addClass("no-poster");
-        }else{
-            var image = $("<img>").attr("src", imgURL);
-            image.addClass("beer-img");
-        }
-
-        beerDiv.append(image);
-        beerDiv.append(pOne,pTwo,pThree,pFour);
-        $("#beer-view").append(beerDiv);
-    }        
-
-    function getMovie(year) {           
-        var queryURL = "https://www.omdbapi.com/?apikey=3f779744&t=beer&y=" + year;  
-        $.ajax({
+function getBeer(newFood) { 
+    var userInput = newFood;
+    var queryURL = "https://api.punkapi.com/v2/beers/?food=" + userInput;
+    $.ajax({
         url: queryURL,
-        method: "GET",
-        }).then(function(response) {             
-            updateMovie(response);
-        });
+        method: "GET"         
+    }).then(function(response) {
+        var random = Math.floor(Math.random()*response.length);
+
+        if (response[random].first_brewed.length === 7){
+            var year = response[random].first_brewed.slice(3);
+        } else{
+            var year = response[random].first_brewed;
+        }
+
+        
+        beerYear.push(year);
+
+        arrayButtons(); 
+        updateBeer(response, random);
+        getMovie(year);            
+    });
+}        
+
+function updateBeer(response,random) {
+    $("#beer-view").empty();
+
+    var beerDiv = $("<div class= 'beer-data'>");
+
+    var name = response[random].name;                      
+    var description = response[random].description;
+    var foodPairing = response[random].food_pairing;
+    var firstBrewedYear = response[random].first_brewed;
+    if (firstBrewedYear.length === 7){
+        firstBrewedYear = response[random].first_brewed.slice(3);
+    } else{
+        firstBrewedYear = response[random].first_brewed;
+    }
+    console.log(response);
+    console.log(response[random].first_brewed);
+
+    var pOne = $("<h5>").html("Beer Name :  " + name);
+    var pTwo = $("<h6>").html("First-brewed Year :  " + firstBrewedYear);           
+    var pThree = $("<h6>").html("Description : " +  '<br>' + description);
+    var pFour = $("<h6>").html("Food Paring : " +  '<br>' + foodPairing);
+    // null is a var not value
+    var imgURL = response[random].image_url;
+    if (imgURL === null){ 
+        var image = $("<p>").text("Beer picture unavailable");
+        image.addClass("no-poster");
+    }else{
+        var image = $("<img>").attr("src", imgURL);
+        image.addClass("beer-img");
     }
 
-    function updateMovie(response) {
-        $("#movie-view").empty();
-        var movieDiv = $("<div class='movie-data'>");          
-        console.log(response);
-        var title = response.Title;
-        var plot = response.Plot;
-        var director = response.Director;
-        var actors = response.Actors;           
-        var country = response.Country;
+    beerDiv.append(image);
+    beerDiv.append(pOne,pTwo,pThree,pFour);
+    $("#beer-view").append(beerDiv);
+}        
+// Where to see the year value that feeds into getMovie
+function getMovie(year) {           
+    var queryURL = "https://www.omdbapi.com/?apikey=3f779744&t=beer&y=" + year;  
+    $.ajax({
+    url: queryURL,
+    method: "GET",
+    }).then(function(response) {             
+        updateMovie(response);
+    });
+}
 
-        var pOne = $("<h5>").html("Title: " + title);
-        var pTwo = $("<h6>").html("Plot: " + plot);
-        var pThree = $("<h6>").html("Director: " + director);
-        var pFour = $("<h6>").html("Actors: " + actors);            
-        var pFive = $("<h6>").html("Country: " + country); 
-        
-        var imgURL = response.Poster;
-        if (imgURL === "N/A"){
-            var image = $("<p>").text("Movie Poster unavailable");
-            image.addClass("no-poster");
-        }else{
-            var image = $("<img>").attr("src", imgURL);  
-            image.addClass("movie-poster");  
-        } 
+function updateMovie(response) {
+    $("#movie-view").empty();
+    var movieDiv = $("<div class='movie-data'>");          
+    console.log(response);
+    var title = response.Title;
+    var plot = response.Plot;
+    var director = response.Director;
+    var actors = response.Actors;           
+    var country = response.Country;
 
-        movieDiv.append(image);
-        movieDiv.append(pOne,pTwo,pThree,pFour,pFive);
+    var pOne = $("<h5>").html("Title: " + title);
+    var pTwo = $("<h6>").html("Plot: " + plot);
+    var pThree = $("<h6>").html("Director: " + director);
+    var pFour = $("<h6>").html("Actors: " + actors);            
+    var pFive = $("<h6>").html("Country: " + country); 
+    
+    var imgURL = response.Poster;
+    if (imgURL === "N/A"){
+        var image = $("<p>").text("Movie Poster unavailable");
+        image.addClass("no-poster");
+    }else{
+        var image = $("<img>").attr("src", imgURL);  
+        image.addClass("movie-poster");  
+    } 
 
-        $("#movie-view").append(movieDiv);
-    }      
+    movieDiv.append(image);
+    movieDiv.append(pOne,pTwo,pThree,pFour,pFive);
+
+    $("#movie-view").append(movieDiv);
+}      
 arrayButtons();   
